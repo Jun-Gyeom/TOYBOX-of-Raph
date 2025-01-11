4ffe77f52ca916a2107a28a7cdc61e81b9e4b95a
Jun-Gyeom <Jun-Gyeom@users.noreply.github.com>
2025-01-12 04:02:50 +0900

    플레이어 제너레이터

--- Assets/02. Scripts/UIManager.cs
+++ Assets/02. Scripts/UIManager.cs
@@ -7,10 +7,6 @@ using System.Drawing;
 
 public class UIManager : Singleton<UIManager>
 {
-    [SerializeField] List<GameObject> healths;      // 체력 UI 
-    [SerializeField] TMP_Text pageText;             // 페이즈 UI
-    [SerializeField] TMP_Text timeText;             // 시간 UI
-    [SerializeField] List<GameObject> dashStacks;   // 대쉬 UI 
     [SerializeField] GameObject fadePanel;          // 페이드 패널 게임오브젝트
     [SerializeField] Image fadeImage;               // 페이드 이미지 
     [SerializeField] GameObject optionPanel;        // 설정 창 
@@ -37,50 +33,6 @@ public class UIManager : Singleton<UIManager>
         Application.Quit();
     }
     #endregion
-
-    // 체력 UI 업데이트
-    public void UpdateHealthUI(int amount)
-    {
-        for (int i = 0; i < healths.Count; i++)
-        {
-            if (i < amount)
-            {
-                healths[i].SetActive(true);
-            }
-            else
-            {
-                healths[i].SetActive(false);
-            }
-        }
-    }
-
-    // 현재 페이즈 UI 업데이트
-    public void UpdatePageText(int pageNum)
-    {
-        pageText.text = $"페이즈 {pageNum.ToString()}";
-    }
-
-    // 현재 시간 UI 업데이트 
-    public void UpdateTimeText(float time)
-    {
-        timeText.text = string.Format("{0:D2}:{1:D2}", time / 60, time % 60);
-    } 
-
-    // 대쉬 스택 UI 업데이트 
-    public void UpdateDashUI(int amount)
-    {
-        for (int i = 0; i < dashStacks.Count; i++)
-        {
-            if (i < amount)
-            {
-                dashStacks[i].SetActive(true);
-            }
-            else
-            {
-                dashStacks[i].SetActive(false);
-            }
-        }
-    }
 
     // 화면 페이드인
     public IEnumerator FadeIn(float duration)
