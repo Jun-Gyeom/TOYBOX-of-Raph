75b81d4edd844195663d53ae46b2c0f6db15d769
wpdudH <66068922+wpdudH@users.noreply.github.com>
2025-01-11 18:30:58 +0900

    대화 UI 추가

---
+++ Assets/02. Scripts/UI/DialogSystem.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Speaker[] speakers;
+    [SerializeField]
+    private DialogData[] dialogs;
+    private bool isAutoStart = true;
+    private bool isFirst = true;
+    private int currentDialogIndex = -1;
+    private int currentSpeakerIndex = 0;
+    private float typingSpeed = 0.1f;
+    private bool isTypingEffect = false;
+
+    void Awake()
+    {
+        
+    }
+
+    private void Setup()
+    {
+
+        for (int i = 0; i < speakers.Length; ++i)
+        {
+            SetActiveObjects(speakers[i], false);
+
+            speakers[i].spriteRenderer.gameObject.SetActive(true);
+        }
+    }
+
+    public bool UpdateDialog()
+    {
+        if (isFirst == true)
+        {
+
+            Setup();
+
+
+            if (isAutoStart) SetNextDialog();
+
+            isFirst = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (isTypingEffect == true)
+            {
+                isTypingEffect = false;
+
+        
+                StopCoroutine("OnTypingText");
+                speakers[currentSpeakerIndex].textDialogue.text = dialogs[currentDialogIndex].dialogue;
+
+                return false;
+            }
+
+            if (dialogs.Length > currentDialogIndex + 1)
+            {
+                SetNextDialog();
+            }
+
+            else
+            {
+      
+                for (int i = 0; i < speakers.Length; ++i)
+                {
+                    SetActiveObjects(speakers[i], false);
+             
+                    speakers[i].spriteRenderer.gameObject.SetActive(false);
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void SetNextDialog()
+    {
+        SetActiveObjects(speakers[currentSpeakerIndex], false);
+
+        currentDialogIndex++;
+
+        currentSpeakerIndex = dialogs[currentDialogIndex].speakerIndex;
+
+        SetActiveObjects(speakers[currentSpeakerIndex], true);
+
+
+        //speakers[currentSpeakerIndex].textDialogue.text = dialogs[currentDialogIndex].dialogue;
+        StartCoroutine("OnTypingText");
+    }
+
+    private void SetActiveObjects(Speaker speaker, bool visible)
+    {
+        speaker.imageDialog.gameObject.SetActive(visible);
+        speaker.textDialogue.gameObject.SetActive(visible);
+
+        Color color = speaker.spriteRenderer.color;
+        color.a = visible == true ? 1 : 0.0f;
+        speaker.spriteRenderer.color = color;
+    }
+
+    private IEnumerator OnTypingText()
+    {
+        int index = 0;
+
+        isTypingEffect = true;
+
+        while (index < dialogs[currentDialogIndex].dialogue.Length)
+        {
+            speakers[currentSpeakerIndex].textDialogue.text = dialogs[currentDialogIndex].dialogue.Substring(0, index);
+
+            index++;
+
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTypingEffect = false;
+    }
+}
+
+[System.Serializable]
+public struct Speaker
+{
+    public Image spriteRenderer;
+    public Image imageDialog;                
+    public TextMeshProUGUI textDialogue;        	
+}
+
+[System.Serializable]
+public struct DialogData
+{
+    public int speakerIndex;        
+    [TextArea(3, 5)]
+    public string dialogue;		
+}
